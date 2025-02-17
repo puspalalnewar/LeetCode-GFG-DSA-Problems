1f8deca74f4583bea2de2fd4cc011084ae91def2
Puspalal <puspalalnewar79@gmail.com>
2025-02-17 22:20:08 +0530

    First Occurence

---
+++ GFG/First Occurence/firstOccurence.java
@@ -0,0 +1,13 @@
+public class firstOccurence {
+
+    static int firstOccurence(String txt, String pat) {
+        if (txt.contains(pat)) {
+            return txt.indexOf(pat);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstOccurence("Hello", "el"));
+    }
+}
---
+++ GFG/Practice/practice.java
@@ -0,0 +1,18 @@
+package GFG.Practice;
+
+import java.util.Scanner;
+
+public class practice {
+
+
+    
+    public static void main(String[] args) {
+        String s = "Puspalal";
+        if(s.contains("us")){
+            System.out.println("Yes");
+            System.out.println(s.indexOf("us"));
+        }else{
+            System.out.println("no");
+        }
+    }
+}
