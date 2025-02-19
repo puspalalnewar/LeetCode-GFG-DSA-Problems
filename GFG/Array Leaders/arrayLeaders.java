e7d7a3dd2a71816234264a653740c41e19fd306d
Puspalal <puspalalnewar79@gmail.com>
2025-02-19 20:27:33 +0530

    Array leader

--- GFG/Array Leaders/arrayLeaders.java
+++ GFG/Array Leaders/arrayLeaders.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 public class arrayLeaders {
 
     static ArrayList<Integer> leaders(int arr[]) {
-        int curr_lea =0;
+        int curr_lea = 0;
         ArrayList<Integer> ans = new ArrayList<Integer>();
-        for(int i=arr.length-1;i>=0;i--){
-            if(arr[i]>=curr_lea){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= curr_lea) {
                 curr_lea = arr[i];
                 ans.add(curr_lea);
             }
@@ -16,6 +18,7 @@ public class arrayLeaders {
     public static void main(String[] args) {
         // Input: arr = [16, 17, 4, 3, 5, 2]
         // Output: [17, 5, 2]
-        // Explanation: Note that there is nothing greater on the right side of 17, 5 and, 2.
+        // Explanation: Note that there is nothing greater on the right side of 17, 5
+        // and, 2.
     }
 }
