fa0a11804c7b2473256bf6e08148259516f2cfc4
Puspalal <puspalalnewar79@gmail.com>
2025-02-21 20:59:58 +0530

    Search in a sorted Matrix

---
+++ GFG/Search in a sorted Matrix/searchInMat.java
@@ -0,0 +1,17 @@
+public class searchInMat {
+
+    public boolean searchMatrix(int[][] mat, int x) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == x) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(5);
+    }
+}
