db6c04d67efa3c00d17adf31802645e679df3346
Puspalal <puspalalnewar79@gmail.com>
2025-03-01 19:33:40 +0530

    String rotated by 2 places

--- GFG/String Rotated by 2 Places/isRotated.java
+++ GFG/String Rotated by 2 Places/isRotated.java
@@ -1,15 +1,15 @@
 public class isRotated {
 
     public static boolean isRotated(String s1, String s2) {
-        if(s1.length() < 2){
+        if (s1.length() < 2) {
             return false;
         }
-        String newStr = s1.substring(s1.length()-2);
-        newStr += s1.substring(0, s1.length()-2);
+        String newStr = s1.substring(s1.length() - 2);
+        newStr += s1.substring(0, s1.length() - 2);
         s1 += s1.substring(0, 2);
-        if(s1.substring(2).equals(s2)){
+        if (s1.substring(2).equals(s2)) {
             return true;
-        }else if(newStr.equals(s2)){
+        } else if (newStr.equals(s2)) {
             return true;
         }
         return false;
@@ -18,7 +18,8 @@ public class isRotated {
     public static void main(String[] args) {
         // Input: s1 = "amazon", s2 = "azonam"
         // Output: true
-        // Explanation: "amazon" can be rotated anti-clockwise by two places, which will make it as "azonam".
+        // Explanation: "amazon" can be rotated anti-clockwise by two places, which will
+        // make it as "azonam".
 
     }
 }
