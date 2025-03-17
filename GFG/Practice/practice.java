280f869ca0401d2f589d7970e383ba5f0f858eed
Puspalal <puspalalnewar79@gmail.com>
2025-03-17 21:10:51 +0530

    Update

--- GFG/Practice/practice.java
+++ GFG/Practice/practice.java
@@ -1,10 +1,22 @@
 package GFG.Practice;
-
 import java.util.*;
 
 public class practice {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+
+        // Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+        // Output: true
+        // Explanation: b[] is a subset of a[]
+
+        int arr[] = {11, 7, 1, 13, 21, 3, 7, 3};
+        int arr2[] = {11, 3, 7, 1, 7};
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i<arr2.length; i++){
+
+        }
+
     }
 }
